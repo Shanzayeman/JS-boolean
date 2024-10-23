55dc0a73fd672d95483c5fdb25b4580c05465f7e
ShanzayEman <shanzayeman0@gmail.com>
2024-10-23 23:12:38 +0500

    Add files via upload

---
+++ 23oct.js
@@ -0,0 +1,9 @@
+document.getElementById("demo1").innerHTML = Boolean(10 > 9);
+const x = new Boolean(false);
+const y = new Boolean(false);
+document.getElementById("demo2").innerHTML = (x == y);
+let
+    X = false;
+let Y = new Boolean(false);
+document.getElementById("demo3").innerHTML = (
+    X == Y);
